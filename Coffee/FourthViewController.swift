7c9ffdb5dbd556ef94cdb4c7acf1be2289b34963
Kirill <syskirill.com>
2024-09-08 14:38:14 +0300

    Onboarding screens were added

---
+++ Coffee/FourthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FourthViewController.swift
+//  Coffee To Go
+//
+//  Created by Кирилл Сысоев on 6.09.24.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    weak var delegate: PageNavigationDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        delegate?.goToNextPage()
+    }
+
+}
