7e0600406d5c93c4d558e5c22a3c771fd24d3ef2
Kirill <syskirill.com>
2024-09-25 18:21:35 +0300

    coffee were added

---
+++ Coffee/DescriptionCoffeeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DescriptionCoffeeViewController.swift
+//  Coffee
+//
+//  Created by Кирилл Сысоев on 25.09.24.
+//
+
+import UIKit
+
+class DescriptionCoffeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Coffee/DrinkCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DrinkCollectionViewCell.swift
+//  Coffee
+//
+//  Created by Кирилл Сысоев on 25.09.24.
+//
+
+import UIKit
+
+class DrinkCollectionViewCell: UICollectionViewCell {
+    
+}
